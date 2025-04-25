ecf7fa8cc69d0fd1bb0dde014a57136aae41529a
sabiasagi <ponter1212@gmail.com>
2025-04-25 20:13:31 +0900

    challange

---
+++ src/app/layout.js
@@ -0,0 +1,46 @@
+import Script from 'next/script';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ja">
+      <head>
+        {/* Typekitスクリプトを head 内に挿入 */}
+        <Script
+          id="typekit-script"  // id 属性を追加
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d) {
+                var config = {
+                  kitId: 'zyl4uuz',
+                  scriptTimeout: 3000,
+                  async: true
+                },
+                h=d.documentElement,
+                t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),
+                tk=d.createElement("script"),
+                f=false,
+                s=d.getElementsByTagName("script")[0],
+                a;
+                h.className+=" wf-loading";
+                tk.src='https://use.typekit.net/'+config.kitId+'.js';
+                tk.async=true;
+                tk.onload=tk.onreadystatechange=function(){
+                  a=this.readyState;
+                  if(f||a&&a!="complete"&&a!="loaded")return;
+                  f=true;
+                  clearTimeout(t);
+                  try{Typekit.load(config)}catch(e){}
+                };
+                s.parentNode.insertBefore(tk,s);
+              })(document);
+            `,
+          }}
+        />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
