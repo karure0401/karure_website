ce6e8d1de6664199412db9855ff093a197dbbc35
sabiasagi <ponter1212@gmail.com>
2025-04-25 19:55:48 +0900

    update1

--- src/app/_document.js
+++ src/app/_document.js
@@ -1,20 +1,43 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
         <Head>
-        <script>
-  (function(d) {
-    var config = {
-      kitId: 'zyl4uuz',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
-</script>
+          {/* Typekitスクリプトの挿入を next/script を使って行う */}
+          <Script
+            strategy="beforeInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(d) {
+                  var config = {
+                    kitId: 'zyl4uuz',
+                    scriptTimeout: 3000,
+                    async: true
+                  },
+                  h=d.documentElement,
+                  t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),
+                  tk=d.createElement("script"),
+                  f=false,
+                  s=d.getElementsByTagName("script")[0],
+                  a;
+                  h.className+=" wf-loading";
+                  tk.src='https://use.typekit.net/'+config.kitId+'.js';
+                  tk.async=true;
+                  tk.onload=tk.onreadystatechange=function(){
+                    a=this.readyState;
+                    if(f||a&&a!="complete"&&a!="loaded")return;
+                    f=true;
+                    clearTimeout(t);
+                    try{Typekit.load(config)}catch(e){}
+                  };
+                  s.parentNode.insertBefore(tk,s);
+                })(document);
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 const works = [
   {
     title: "サンプル1",
@@ -14,36 +16,42 @@ const works = [
 
 export default function Home() {
   return (
-    <div className="flex justify-right min-h-screen" >
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ml-2 mt-20 ">
+    <div className="flex justify-right min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ml-2 mt-20">
         {works.map((work, i) => (
           <div
-          key={i}
-          className="p-0.5 w-80 h-45 bg-neutral-900/0 hover:bg-neutral-900/20 transform hover:scale-102 transition-all duration-300"
-        >
-          <img src={work.image} alt={work.title} className="w-full h-auto rounded" />
-          <div
-            className="mt-2"
-            style={{
-              fontFamily: "sans-serif",
-              color: "#E6E6E6 hover:text-blue-400 transition-all duration-300 ease-in-out",
-              fontSize: "16px"
-            }}
+            key={i}
+            className="p-0.5 w-80 h-45 bg-neutral-900/0 hover:bg-neutral-900/20 transform hover:scale-102 transition-all duration-300"
           >
-            {work.title}
+            {/* next/image を使用して画像を最適化 */}
+            <Image
+              src={work.image}
+              alt={work.title}
+              width={320} // 任意の幅
+              height={180} // 任意の高さ
+              className="w-full h-auto rounded"
+            />
+            <div
+              className="mt-2"
+              style={{
+                fontFamily: "sans-serif",
+                color: "#E6E6E6 hover:text-blue-400 transition-all duration-300 ease-in-out",
+                fontSize: "16px",
+              }}
+            >
+              {work.title}
+            </div>
+            <div
+              className="text-xs"
+              style={{
+                fontFamily: "Futura PT Light",
+                color: "#E6E6E6",
+                fontSize: "8px",
+              }}
+            >
+              {work.date}
+            </div>
           </div>
-          <div
-            className="text-xs"
-            style={{
-              fontFamily: "Futura PT Light",
-              color: "#E6E6E6",
-              fontSize: "8px"
-            }}
-          >
-            {work.date}
-          </div>
-        </div>
-        
         ))}
       </div>
     </div>
